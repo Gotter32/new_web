29fe843d05f53aab4a463c49e61ed0c587a0ae84
Gotter32 <164605740+Gotter32@users.noreply.github.com>
2024-03-30 16:45:39 +0700

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="header_main">
+            <h1>Рабочая зона</h1>
+        </div>
+    )
+}
+
+export default Header
